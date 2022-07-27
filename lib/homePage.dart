12b3a7eebcb5331265418237f917a554ec71afef
leul <leulwebshet@yhaoo.com>
2022-07-27 22:10:36 +0300

    Task Done

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -14,64 +14,72 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final _pickImage = ImagePicker();
-  File? imagePath;
-  late List _result;
-  // late String _confindnce;
-
-  String _name = '';
-  String _number = '';
+  //Modifed code
+  bool _loading = true;
+  File? _image;
+  late List _output;
+  final picker = ImagePicker();
 
   @override
   void initState() {
     super.initState();
-    loadModel();
+    importModel();
   }
 
-  fromCamera() async {
-    final XFile? image = await _pickImage.pickImage(source: ImageSource.camera);
-
-    if (image != null) {
-      setState(() {
-        imagePath = File(image.path);
-      });
-    }
-  }
-
-  fromGallary() async {
-    final XFile? image =
-        await _pickImage.pickImage(source: ImageSource.gallery);
-
-    if (image != null) {
-      setState(() {
-        imagePath = File(image.path);
-      });
-    }
-  }
-
-  Future loadModel() async {
-    Tflite.close();
-    var result = await Tflite.loadModel(
-        model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
-  }
-
- Future applyModel(File file) async {
-    var res = await Tflite.runModelOnImage(
-        path: file.path,
-        numResults: 2, //Change
+  classifyImage(File img) async {
+    var outcome = await Tflite.runModelOnImage(
+        path: img.path,
+        numResults: 4,
         threshold: 0.5,
         imageMean: 127.5,
         imageStd: 127.5);
 
     setState(() {
-      _result = res!;
-
-      // Code need to be understood
-      String str = _result[0]["label"];
-      _name = str.substring(2);
-      // _confindnce = _result != null?(_result[0]['Confidence']*100.0.toString().substring(0,2)+'%'):"";
+      _output = outcome!;
+      _loading = false;
     });
   }
+
+  importModel() async {
+    await Tflite.loadModel(
+        model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    Tflite.close();
+  }
+
+  pickImage() async {
+    // var photo = await picker.pickImage(source: ImageSource.camera);
+    final XFile? photo = await picker.pickImage(source: ImageSource.gallery);
+
+    //checking for error
+    if (photo == null) return null;
+
+    setState(() {
+      _image = File(photo.path);
+    });
+
+    classifyImage(_image!);
+  }
+
+  pickGallery() async {
+    // var photo = await picker.pickImage(source: ImageSource.gallery);
+    final XFile? photo = await picker.pickImage(source: ImageSource.gallery);
+    //checking for error
+    if (photo == null) return null;
+
+    setState(() {
+      _image = File(photo.path);
+    });
+
+    classifyImage(_image!);
+  }
+
+  // End of Modefied code
 
   @override
   Widget build(BuildContext context) {
@@ -84,12 +92,12 @@ class _HomeState extends State<Home> {
           backgroundColor: Color(0xFFFF8242),
         ),
         body: Container(
-          child: imagePath == null
+          child: _image == null
               ? Text('Select Image')
               : Column(
                   children: [
-                    Image.file(File(imagePath!.path)),
-                    Text('Status:$_name'),
+                    Image.file(File(_image!.path)),
+                    Text('${_output[0]}'),
                   ],
                 ),
         ),
@@ -98,7 +106,7 @@ class _HomeState extends State<Home> {
           FloatingActionButton(
             backgroundColor: Color(0xFFFF8242),
             onPressed: () {
-              fromCamera();
+              pickImage();
             },
             child: Icon(Icons.camera_alt_outlined),
           ),
@@ -108,7 +116,7 @@ class _HomeState extends State<Home> {
           FloatingActionButton(
               backgroundColor: Color(0xFFFF8242),
               onPressed: () {
-                fromGallary();
+                pickGallery();
               },
               child: Icon(Icons.browse_gallery_outlined))
         ]),
